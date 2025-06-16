f745ea623b52752e0232eb519047820b10e91b5d
SIBILLE MAURIZIO <maurizio.sibille@esternibisp.com>
2025-06-16 17:52:11 +0200

    First commit

---
+++ src/test/java/it/msibille/mongo_spring/MongoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package it.msibille.mongo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
